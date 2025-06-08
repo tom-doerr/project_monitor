671a1c45c797c99f68b049f2a4144f11e754e7b7
Tom Dörr <tomdoerr96@gmail.com>
2025-06-08 09:14:30 +0200

    chore: update .gitignore and normalize setup.py quotes

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="project_watch",
     version="0.1.0",
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         "pylint",
         "pytest",
